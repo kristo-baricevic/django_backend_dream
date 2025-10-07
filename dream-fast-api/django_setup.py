223162a548ebd441ff36db095b31a03037bba34f
Kristo Baricevic <kristo.baricevic@gmail.com>
2025-10-07 15:22:02 -0400

    update

--- dream-fast-api/django_setup.py
+++ dream-fast-api/django_setup.py
@@ -2,7 +2,13 @@ import os
 import sys  
 import django
 
-sys.path.insert(0, '/django_backend_dream')
+# Detect environment
+if os.path.exists('/django_backend_dream'):
+    # Docker environment
+    sys.path.insert(0, '/django_backend_dream')
+else:
+    # Local environment
+    sys.path.insert(0, '/Users/kristo/django_backend_dream')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
